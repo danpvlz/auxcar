7018801dbe530900935f05584a2aa217a1759e2d
Daniela <danielapv200@gmail.com>
2020-09-20 22:21:36 -0500

    Mensaje

--- src/views/partials/Finalizado.js
+++ src/views/partials/Finalizado.js
@@ -3,22 +3,24 @@ import {Link} from 'react-router-dom';
 
 import {
     Card,
+    CardHeader,
     CardBody,
     CardFooter,
     Button,
     Col
 } from 'reactstrap'
 
-class Finalizado extends React.Component {
+class ResultadoEncuesta extends React.Component {
   render() {
     return (
       <Col lg="5">
         <div className="animate__animated animate__slideInDown animate__fast">
         <Card className="bg-gradient-success" sm="4">
             <CardBody className="text-white">
-                <div className="py-3 text-center">
+                <div className="text-center">
                 <i className="ni ni-like-2 ni-3x"/>
-                <p className="heading mt-5">Solicitud registrada con éxito</p>  
+                <p className="mt-5"><b>Felicidades! Su solicitud de auxilio ha sido registrada</b></p>  
+                <p>Mantenga su celular a la mano, que en los próximos minutos uno de nuestros operadores lo estará llamando.</p>  
                 </div>
             </CardBody>
             <CardFooter className="bg-gradient-success">
@@ -40,5 +42,5 @@ class Finalizado extends React.Component {
   }
 }
 
-export default Finalizado;
+export default ResultadoEncuesta;
                   
